78b909362257f58b9993736134717e89c0b25b46
Harshit221 <harshitvaghani221@gmail.com>
2020-03-14 09:33:52 +0000

    [LocalTest]:[MergeIntervals]:[java]

--- MergeIntervals/MergeIntervals.java
+++ MergeIntervals/MergeIntervals.java
@@ -3,74 +3,71 @@ import java.util.*;
 
 class MergeIntervals {
     // Implement your solution by completing the below function
-    public int[][] merge(int[][] intervals, int max) {
-        int range[] = new int[max+2];
-        for(int i = 0; i<intervals.length; i++) {
-            range[intervals[i][0]]++;
-            range[intervals[i][1] + 1]--;
-        }
-        for(int i = 1; i < range.length; i++) {
-            range[i] += range[i-1];
-        }
-        int flag = 0, start = 0, end = 0;
-        List<Pair<Integer, Integer> > pairList = new ArrayList(); 
-        for(int i = 1; i<range.length; i++) {
-            if(flag ==0) {
-                if(range[i] != 0 && range[i-1] == 0) {
-                    start = i;
-                    flag = 1;
-                }                    
-            } else {
-                if(range[i] != 0 && range[i+1] == 0) {
-                    end = i;
-                    flag = 0;
-                    pairList.add(new Pair(start, end));
-                }
+    public List<Pair> merge(Pair[] intervals) {
+        
+        Arrays.sort(intervals, new Comparator<Pair>() {
+            public int compare(Pair a, Pair b) {
+                return Integer.compare(a.getStart(), b.getStart());
             }
-
+        });
+        List<Pair> pairs = new ArrayList<>();
+        int size = 0;
+        pairs.add(intervals[0]);
+        for(int i = 1;i<intervals.length; i++) {
+            if(intervals[i].getStart() <= pairs.get(size).getEnd() ) {
+                pairs.get(size).setEnd(Math.max(pairs.get(size).getEnd(), intervals[i].getEnd()));
+            } else {
+                pairs.add(intervals[i]);
+                size++;
+            }
         }
-        int[][] result = new int[pairList.size()][2];
-        for(int i = 0; i < pairList.size(); i++) {
-            result[i][0] = pairList.get(i).getKey();
-            result[i][1] = pairList.get(i).getValue();
-        }
-        return result;
+        return pairs;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[][] nums = new int[n][2];
-        int max = -1;
+        Pair[] nums = new Pair[n];
+        int x, y;
         for (int i = 0; i < n; i++) {
-            nums[i][0] = scanner.nextInt();
-            nums[i][1] = scanner.nextInt();
-            if(max < nums[i][1]) {
-                max = nums[i][1];
-            }
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            nums[i] = new Pair(x, y);
         }
         scanner.close();
 
-        int[][] results = new MergeIntervals().merge(nums, max);
-        for (int i = 0; i < results.length; ++i)
-            System.out.println(String.valueOf(results[i][0]) + " " + String.valueOf(results[i][1]));
+        List<Pair> results = new MergeIntervals().merge(nums);
+        for (int i = 0; i < results.size(); ++i)
+            System.out.println(results.get(i).getStart() + " " + results.get(i).getEnd());
     }
     
-    private class Pair<T, U> {
-        private T key;
-        private U value;
+    private static class Pair {
+        int start;
+        int end;
 
-        Pair(T key, U value) {
-            this.key = key;
-            this.value = value;
+        public Pair(int s, int e) {
+            start = s;
+            end = e;        
         }
 
-        T getKey() {
-            return this.key;
+        public int getStart() {
+            return start;
         }
 
-        U getValue() {
-            return this.value;
+        public void setStart(int start) {
+            this.start = start;
         }
+
+        public int getEnd() {
+            return end;
+        }
+
+        public void setEnd(int end) {
+            this.end = end;
+        }
+
+        
+        
     }
+    
 }
