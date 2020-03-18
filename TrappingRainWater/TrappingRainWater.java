f1ef22d7ed4dac81962e0663725c16b27f9987d9
Harshit221 <harshitvaghani221@gmail.com>
2020-03-18 10:56:07 +0000

    [LocalTest]:[TrappingRainWater]:[java]

--- TrappingRainWater/TrappingRainWater.java
+++ TrappingRainWater/TrappingRainWater.java
@@ -3,8 +3,33 @@ import java.util.*;
 class TrappingRainWater {
 
     // complete the below function implementation
+    public int start(int[] a) {
+        for(int i=0;i<a.length;i++)
+            if(a[i]>0)
+                return i;
+        return -1;
+    }
+    public int end(int[] a) {
+        for(int i=a.length;i>=0;i--)
+            if(a[i]>0)
+                return i;
+        return -1;
+    }
     public int trap(int[] height) {
-        return 0;
+        int count;
+        int left=0,right,length;
+        while( (left = start(a)) != -1 ){
+            right = end(a);
+            for(int i=left;i<right;i++) {
+                if(a[i]==0)
+                    count++;
+                else
+                    a[i]--;
+            }
+            
+
+        }
+        return count;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
