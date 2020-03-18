b736ce8a1bf5cc680cffd598742b486acf4a9db8
Harshit221 <harshitvaghani221@gmail.com>
2020-03-18 09:53:48 +0000

    [LocalTest]:[NumberOfIslands]:[java]

--- NumberOfIslands/NumberOfIslands.java
+++ NumberOfIslands/NumberOfIslands.java
@@ -3,10 +3,41 @@ import java.util.*;
 
 class NumberOfIslands {
 
+    static class GetAxis {
+        static int[] xAxis = { 1, 0, -1, 0 };
+        static int[] yAxis = { 0, 1, 0, -1 };
+    }
+
+    public void bfs(int i,int j,char arr[][]){
+	    arr[i][j] = '0';
+	    for(int k=0;k<4;k++){
+	        int x = i + GetAxis.xAxis[k];
+	        int y = j + GetAxis.yAxis[k];
+	        
+	        if(x<0 || x>=arr.length || y<0 || y>= arr[0].length)
+	            continue;
+	        if(arr[x][y] == '1')
+                bfs(x,y,arr);
+	    }
+	}
+
     // Implement your solution by completing the below function
     public int numIslands(char[][] grid) {
-
-        return 0;
+        if(grid.length == 0) {
+            return 0;
+        }
+        int n = grid[0].length;
+        int m = grid.length;
+        int cnt = 0;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == '1') {
+                    cnt++;
+                    bfs(i,j,grid);
+                }
+            }
+        } 
+        return cnt;
     }
 
     public static void main(String[] args) {
