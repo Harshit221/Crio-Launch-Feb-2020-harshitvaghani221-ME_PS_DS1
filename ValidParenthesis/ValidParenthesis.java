ccc97f425d95f70b137d9f375d8100082660fac6
Harshit221 <harshitvaghani221@gmail.com>
2020-03-18 10:24:55 +0000

    [LocalTest]:[ValidParenthesis]:[java]

--- ValidParenthesis/ValidParenthesis.java
+++ ValidParenthesis/ValidParenthesis.java
@@ -3,18 +3,31 @@ import java.util.*;
 
 public class ValidParenthesis {
     // Implement your solution by completing the below function
+    public boolean isOpening(char c) {
+      if(c == '(' || c == '{' || c == '[')
+        return true;
+      else
+        return false;
+      
+    }
     public boolean isValid(String s) {
       Map<Character, Character> map = new HashMap<>();
       
       map.put('(',')');
       map.put('{','}');
       map.put('[',']');
+      Stack<Character> stack = new Stack<>();
       int length = s.length();
       if(length%2==1)
         return false;
-      for(int i=0;i<length/2;i++) {
-        if(map.get(s.charAt(i)) != s.charAt(length-i-1))
-          return false;
+      for(int i=0;i<length;i++) {
+        char c = s.charAt(i);
+        if(isOpening(c)){
+          stack.push(map.get(c));
+        } else {
+          if(stack.pop() != c)
+            return false;            
+        }
       }
       return true;
     }
