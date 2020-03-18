ed865984adbcc58448c4706cf8f8e88b0a6ca51f
Harshit221 <harshitvaghani221@gmail.com>
2020-03-18 10:17:04 +0000

    [LocalTest]:[ValidParenthesis]:[java]

--- ValidParenthesis/ValidParenthesis.java
+++ ValidParenthesis/ValidParenthesis.java
@@ -4,8 +4,18 @@ import java.util.*;
 public class ValidParenthesis {
     // Implement your solution by completing the below function
     public boolean isValid(String s) {
-
-		return false;
+      Map<Character, Character> map = new HashMap<>();
+      map.add('(',')');
+      map.add('{','}');
+      map.add('[',']');
+      int length = s.length();
+      if(length()%2==1)
+        return false;
+      for(int i=0;i<length/2;i++) {
+        if(map.get(s.charAt(i)) != s.charAt(length-i-1))
+          return false;
+      }
+      return true;
     }
 
     public static void main(String[] args) throws IOException {
